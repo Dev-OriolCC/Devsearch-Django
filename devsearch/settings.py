2fbea3f942986fd6436896719a5e38c3c3874adf
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2021-08-25 23:47:49 -0500

    Added static files to project

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-=yqw+!oy@iomj6qn+xi8ojswxrg(evv$%u_7ajm8x**jw&063a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,7 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.projects, name="projects"),
     path('project/<str:pk>', views.project, name="project"),
     path('project-create', views.project_create, name="project_create"),
+    path('project-update/<str:pk>', views.project_update, name="project_update"),
+    path('project-delete/<str:pk>', views.project_delete, name="project_delete"),
 ]
