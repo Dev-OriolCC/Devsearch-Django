44c1243bd87148745d9ca4e483b6539c1cf430a6
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2021-08-29 20:55:50 -0500

    Added images & users app

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Add apps
     'projects.apps.ProjectsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
--- devsearch/urls.py
+++ devsearch/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projects.urls')) #Conect this with the other urls file in projects#
+    path('projects/', include('projects.urls')), #Conect this with the other urls file in PROJECTS#
+    path('', include('users.urls')) #Conect this with the other urls file in USERS#
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
